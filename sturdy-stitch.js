a523ca2db1ebf3d9f7a1fb2b5ec4e75ab02b7d9a
Steve Coffman <gears@umich.edu>
2016-11-17 17:16:36 -0500

    Fix eslint to be cool

--- sturdy-stitch.js
+++ sturdy-stitch.js
@@ -5,11 +5,11 @@ var app = express();
 
 // pretty sure the cookieDomainRewrite is correct, but might be backwards
 app.use('/', proxy({target: 'http://firefly.jstor.org',
-    changeOrigin: true,
-    autoRewrite: true,
-    protocolRewrite: true,
-    cookieDomainRewrite: { "localhost": "firefly.jstor.org"}
-  }));
+	changeOrigin: true,
+	autoRewrite: true,
+	protocolRewrite: true,
+	cookieDomainRewrite: { 'localhost': 'firefly.jstor.org'}
+}));
 app.listen(4000);
 
 // http://localhost:3000/api/foo/bar -> http://www.example.org/api/foo/bar
